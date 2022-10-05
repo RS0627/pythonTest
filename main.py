02bf0b325cb86f28d541ce98fa4584e1779838e1
RyotaSakai <neko0627@gmail.com>
2022-10-05 22:59:42 +0900

    excel行分析を記載

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("hello world!")
+import pandas as pd
 
-print("second message.")
+print("hello world!")
+print(pd.__version__)
+
+df = pd.read_excel('./サンプルデータ.xlsx', skiprows=1)
+print(df)
+print('***********************')
+
+for row in df.itertuples():
+    print(row[1], row[2])
+# print(df.loc[1])
